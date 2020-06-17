8de541dbcc2b18182c80b82303af063092e7f121
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-06-17 13:20:14 -0400

    added a delete button in index.html and delete book function on app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,24 @@
 const app = angular.module("BooksApp", []);
 
 app.controller("MainController", [
-  "$http",
-  function ($http) {
-    this.hello = "Hello World!";
-  },
-]);
+  "$http", function ($http) {
+
+  const controller = this
+
+  this.deleteBook = function(book) {
+    $http(
+      {
+        method: 'DELETE',
+        route: '/books/' + book._id
+      }
+    ).then(
+      function(response){
+        controller.getBooks();
+      },
+      function(error){
+        console.log(error);
+      }
+    )
+  }
+
+}]);
